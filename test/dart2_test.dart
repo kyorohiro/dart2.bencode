a5f92f38bb3a03fa9f2afed07ca30f4c1df77b48
kyorohiro <kyorohiro@gmail.com>
2021-04-08 04:25:12 +0900

    first commit

---
+++ test/dart2_test.dart
@@ -0,0 +1,16 @@
+import 'package:dart2.bencode/dart2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
